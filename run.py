76f2fe2edd9a7118ccc345ffe80c61aa9d3e8bd4
garymast <>
2023-07-08 12:28:11 +0000

    Add main return

--- run.py
+++ run.py
@@ -135,10 +135,10 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
-
+    return stock_data
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
 
 stock_values = get_stock_values(stock_data)
 # Write you code below this comment
